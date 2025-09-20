1086b35295d0c15645a95e1a7a6315d62fdce327
Saurabh <saurabh.d2106@gmail.com>
2025-09-20 13:36:25 +0530

    Sample Node App Code

---
+++ jest.config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  reporters: [
+    'default',
+    ['jest-junit', {
+      outputDirectory: 'coverage',
+      outputName: 'junit.xml'
+    }]
+  ],
+  projects: [
+    {
+      displayName: 'server',
+      testEnvironment: 'node',
+      testMatch: [
+        '**/tests/server.test.js',
+        '**/tests/integration.test.js'
+      ],
+      collectCoverageFrom: [
+        'server.js',
+        '!**/node_modules/**',
+        '!**/coverage/**'
+      ],
+      coverageDirectory: 'coverage/server',
+      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+    },
+    {
+      displayName: 'client',
+      testEnvironment: 'jsdom',
+      testMatch: [
+        '**/tests/client.test.js'
+      ],
+      collectCoverageFrom: [
+        'public/script.js',
+        '!**/node_modules/**',
+        '!**/coverage/**'
+      ],
+      coverageDirectory: 'coverage/client',
+      setupFilesAfterEnv: ['<rootDir>/tests/setup.js']
+    }
+  ]
+};
